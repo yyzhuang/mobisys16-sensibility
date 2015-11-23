b58997efd681c823b1e25f993da90b2d4aebfe6d
yyzhuang <yanyanzhuang83@gmail.com>
2015-11-23 00:12:38 -0800

    acc new script

--- acc_experiment/calc.py
+++ acc_experiment/calc.py
@@ -1,24 +1,32 @@
 import re
 import matplotlib.pyplot as plt
 import os
+import math
+import random
+import numpy
+import sys
 
+directory = sys.argv[1]  # specify a subdir
 g = 9.8  # gravity
 
-zup = []
-with open('acclogup.txt') as f:
-  for line in f:
-    line = re.sub('[[]', '', line)
-    line = re.sub('[]]', '', line).rstrip()
-    x, y, z = line.split(",")
-    zup.append(float(z))
+def getz(file):
+  zvalue = []
+  with open(file) as f:
+    for line in f:
+      line = re.sub('[[]', '', line)
+      line = re.sub('[]]', '', line).rstrip()
+      x, y, z = line.split(",")
+      #zvalue.append(float(z))
+      zvalue.append(float(z) * math.cos(random.expovariate(random.random())))
+  return zvalue
 
-zdown = []
-with open('acclogdown.txt') as f:
-  for line in f:
-    line = re.sub('[[]', '', line)
-    line = re.sub('[]]', '', line).rstrip()
-    x, y, z = line.split(",")
-    zdown.append(float(z))
+def savedata(file, data):
+  f = open(file, "w")
+  f.write(str(data)) 
+  f.close()
+    
+zup = getz(directory + '/acclogup.txt')
+zdown = getz(directory + '/acclogdown.txt')
 
 if len(zup) != len(zdown):
   print "zup and zdown should have the same size..."
@@ -26,18 +34,24 @@ if len(zup) != len(zdown):
 
 sz = []
 oz = []
-
 for i in xrange(len(zup)):
  sz.append((zup[i] - zdown[i])/(2*g)) 
  oz.append((zup[i] + zdown[i])/2) 
 
-f1 = open(os.getcwd().split('/')[-1] + '-sz.txt', "w")
-f1.write(str(sz)) 
-f1.close()
+savedata(directory + '-sz-rand.txt', sz)
+savedata(directory + '-oz-rand.txt', oz)
 
-f2 = open(os.getcwd().split('/')[-1] + '-oz.txt', "w")
-f2.write(str(oz)) 
-f2.close()
+print numpy.mean(sz)
+print numpy.median(sz)
+print numpy.std(sz)
+print numpy.var(sz)
+print 
 
-plt.scatter(sz, oz, c='red')
+print numpy.mean(oz)
+print numpy.median(oz)
+print numpy.std(oz)
+print numpy.var(oz)
+
+
+plt.scatter(sz, oz, c='yellow')
 plt.show()
